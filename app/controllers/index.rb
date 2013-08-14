cd4030db3d1648327e7ed1c88d4aa4af1e2afaf8
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-14 15:49:17 -0500

    added signup, create new post, create new comment routes and views

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,77 @@
 get '/' do
-  # Look in app/views/index.erb
+  @posts = Post.order('created_at DESC')
   erb :index
 end
+
+get '/user/signup' do
+  erb :signup
+end
+
+post '/user/signup' do
+  @user = User.new(params[:user])
+    if @user.save 
+      session[:user_id] = @user.id
+      redirect '/'
+    else
+      redirect 'user/signup'
+    end
+end
+
+post '/user/signin' do
+  @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+    else
+      redirect "/"
+    end
+end
+
+get '/user/signout' do
+
+  session[:user_id] = nil
+  redirect '/'
+end
+
+get '/posts/create' do
+  erb :new_post
+end
+
+post '/posts/create' do
+  @post = Post.create(title: params[:post][:title], body: params[:post][:body], user_id: current_user.id)
+  redirect '/'
+end
+
+get '/posts/:post_id' do
+  @post = Post.find(params[:post_id])
+  @comments = @post.comments
+  erb :post
+end
+
+post '/post/:post_id/comment/create' do
+  @post = Post.find(params[:post_id])
+  @post.comments << Comment.create(body: params[:comment][:body], user_id: current_user.id)
+  redirect '/'
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
 
   has_many :posts
   has_many :comments
+
+  def authenticate(unencrypted_password)
+    BCrypt::Password.new(password_digest) == unencrypted_password && self
+  end
+
+
 end
